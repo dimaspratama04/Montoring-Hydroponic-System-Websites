185df38b345724312ca399ddc58e626d0dd42be2
Dimas <madimas04@gmail.com>
2023-02-17 16:33:11 +0700

    sisa dashboard

--- route/data/getAllDevices.js
+++ route/data/getAllDevices.js
@@ -3,15 +3,30 @@ const db = require("../../utils/databaseConfig");
 const getAllDevices = (req, res) => {
   try {
     const deviceKey = req.query.key;
-    const queryGetDevices = `SELECT * FROM devices WHERE deviceKey = '${deviceKey}'`;
-    db.query(queryGetDevices, (err, results) => {
-      if (err) throw err;
-      if (results.length > 0) {
-        res.json(results);
-      } else {
-        res.json({ statusText: "ERROR", message: "Devices not found !" });
-      }
-    });
+    // Dashboard
+    if (req.query.state === "dashboard") {
+      const queryGetDevicesDashboard = `SELECT * FROM devices INNER JOIN datas ON devices.deviceKey = datas.deviceKey WHERE devices.deviceKey = '${deviceKey}'`;
+      db.query(queryGetDevicesDashboard, (err, results) => {
+        if (err) throw err;
+        if (results.length > 0) {
+          res.json(results);
+        } else {
+          res.json({ statusText: "ERROR", message: "Devices not found !" });
+        }
+      });
+
+      // Device info
+    } else if (req.query.state === "info") {
+      const queryGetDevices = `SELECT * FROM devices WHERE deviceKey = '${deviceKey}'`;
+      db.query(queryGetDevices, (err, results) => {
+        if (err) throw err;
+        if (results.length > 0) {
+          res.json(results);
+        } else {
+          res.json({ statusText: "ERROR", message: "Devices not found !" });
+        }
+      });
+    }
   } catch (e) {
     res.json({ statusText: "ERROR", message: "Something wrong !" });
   }
