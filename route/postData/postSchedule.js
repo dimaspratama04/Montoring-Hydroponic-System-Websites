fd2d3270a5be353891cf0ce5b497c56af0430a5b
Dimas <madimas04@gmail.com>
2023-02-27 16:47:39 +0700

    dashboard update

--- route/postData/postSchedule.js
+++ route/postData/postSchedule.js
@@ -3,20 +3,28 @@ const mqtt = require("mqtt");
 const db = require("../../utils/databaseConfig");
 
 const postSchedule = (req, res) => {
-  let { deviceKey, topicSchedule, on, off } = req.body;
+  let { deviceKey, deviceName, topicSchedule, on, off } = req.body;
 
-  const queryCheckDeviceKey = `SELECT * FROM devices WHERE deviceKey = '${deviceKey}'`;
+  const queryCheckDeviceKey = `SELECT * FROM devices WHERE deviceKey = '${deviceKey}' AND  deviceName = '${deviceName}'`;
   db.query(queryCheckDeviceKey, (err, results) => {
     if (err) throw err;
     if (results.length > 0) {
       let sendSchedule = [on, off];
       const client = mqtt.connect("mqtt://broker.emqx.io:1883");
-      client.on("connect", () => {
+      client.on("connect", (err) => {
         client.publish(`${topicSchedule}`, JSON.stringify(sendSchedule));
-        res.send("SUCCES !");
+        res.send("SUCCES : Success send schedule to devices !");
+
+        // Insert to log db
+        sendSchedule.map((log) => {
+          db.query("INSERT INTO logschedule(`deviceKey`,`log`) VALUE ( ?,? )", [deviceKey, log], (err, results) => {
+            if (err) throw err;
+            console.log(results);
+          });
+        });
       });
     } else {
-      res.send("DEVICE KEY IS NOT VALID !");
+      res.send("DEVICE KEY OR DEVICE NAME IS NOT VALID !");
     }
   });
 };
