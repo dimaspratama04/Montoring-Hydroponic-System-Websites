0cc71e79ef76acc405b59fcaf60a10c3aa0fdd18
Dimas <madimas04@gmail.com>
2023-04-15 14:08:02 +0700

    update user register method

--- route/register/userRegister.js
+++ route/register/userRegister.js
@@ -1,21 +1,21 @@
 const db = require("../../utils/databaseConfig");
 
 const userRegister = (req, res) => {
-  let { username, password, email } = req.body;
+  const { username, password, email } = req.body;
 
-  const queryUserRegister = "INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)";
-  if (username && password && email) {
-    db.query(queryUserRegister, [username, password, email], (err, results) => {
-      if (err) {
-        throw err;
-      } else {
-        res.redirect("/");
-      }
-    });
-  } else {
-    res.redirect("/");
-    res.end();
-  }
+  const queryCheckUsername = `SELECT username FROM accounts WHERE username = '${username}' `;
+  db.query(queryCheckUsername, (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.status(409).json({ statusText: "CONFLICT", message: "Username Already Exist !" });
+    } else {
+      const queryUserRegister = `INSERT INTO accounts (username, password, email) VALUES ('${username}','${password}','${email}')`;
+      db.query(queryUserRegister, (err, results) => {
+        if (err) throw err;
+        res.status(200).json({ statusText: "SUCCES", message: "Account success created !" });
+      });
+    }
+  });
 };
 
 module.exports = userRegister;
